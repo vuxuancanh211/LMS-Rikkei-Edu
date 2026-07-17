f54969d0743fcf7c423971a7b5a0ff0f9d7cadb9
quoc1106 <quocminhtran24032004@gmail.com>
2026-07-17 13:42:29 +0700

    update conflict

--- backend/src/test/java/project/lms_rikkei_edu/modules/assignment/service/impl/AssignmentServiceImplTest.java
+++ backend/src/test/java/project/lms_rikkei_edu/modules/assignment/service/impl/AssignmentServiceImplTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.util.ReflectionTestUtils;
 import project.lms_rikkei_edu.common.exception.BusinessException;
@@ -55,6 +57,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class AssignmentServiceImplTest {
 
     @Mock
